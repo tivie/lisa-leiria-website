d6390dd7575b86a9549fd45aacb037e8c12b9ecc
Estevão Soares dos Santos <estevao.santos@gmail.com>
2022-11-12 00:19:20 +0000

    add google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import { defineConfig } from 'vite';
 import * as path from "path";
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import ViteRadar from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+      vue(),
+    vueJsx(),
+    ViteRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-VKR2JB9VM3'
+      },
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
